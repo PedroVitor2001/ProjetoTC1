7a692aeb7a1f374592a789fced9294c0587de2c0
Pedro Petrilli <ppetrilli96@gmail.com>
2024-06-10 14:03:07 -0300

    test: should show items at responsive menu

--- test/src/test/java/org/example/tests/ResponsiveElementTests.java
+++ test/src/test/java/org/example/tests/ResponsiveElementTests.java
@@ -3,8 +3,11 @@ package org.example.tests;
 import org.example.pages.HomePage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ResponsiveElementTests extends BaseTest {
 
@@ -19,5 +22,29 @@ public class ResponsiveElementTests extends BaseTest {
         Thread.sleep(5000);
 
         assertThat(homePage.getResponsiveElement().isDisplayed()).isTrue();
+    }
+
+    @Test
+    void shouldShowMenuItemsAfterButtonClickAt767Width() throws InterruptedException {
+
+        HomePage homePage = new HomePage(driver);
+
+        driver.manage().window().setSize(new org.openqa.selenium.Dimension(767, 800));
+
+        assertThat(homePage.getResponsiveElement().isDisplayed()).isTrue();
+
+        WebElement button = driver.findElement
+                (By.xpath("/html/body/div[1]/div/div[1]/div/button"));
+        button.click();
+
+        Thread.sleep(5000);
+
+        WebElement homeLink = driver.findElement
+                (By.xpath("/html/body/div[1]/div/div[1]/div[2]/nav/a[1]"));
+        WebElement listLink = driver.findElement
+                (By.xpath("/html/body/div[1]/div/div[1]/div[2]/nav/a[2]"));
+
+        assertTrue(homeLink.isDisplayed());
+        assertTrue(listLink.isDisplayed());
     }
 }
