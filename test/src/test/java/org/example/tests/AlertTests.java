33cf2bb619619c9642c448eedbe6f90c700c03b6
SandroFerreiraDev <sandro.ferreira.developer@gmail.com>
2024-06-11 16:39:23 -0300

    test: added listpage alert test

--- test/src/test/java/org/example/tests/AlertTests.java
+++ test/src/test/java/org/example/tests/AlertTests.java
@@ -1,6 +1,7 @@
 package org.example.tests;
 
 import org.example.pages.HomePage;
+import org.example.pages.ListPage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Alert;
@@ -16,6 +17,30 @@ public class AlertTests extends BaseTest{
         HomePage homePage = new HomePage(driver);
 
         homePage.getButtonLearnMore().click();
+
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+
+
+        String alertText = alert.getText();
+        String expectedText = "Esta aplicação focada em mobile visa ser um fácil cadastro para leituras futuras. " +
+                "Desenvolvida por: Lucas Sigoli, João Castro e Erick Gomes";
+        assertThat(alertText).isEqualTo(expectedText);
+        Thread.sleep(5000);
+
+        alert.accept();
+
+    }
+
+    @Test
+    @DisplayName("Should display info alert when clicking on info button at list page")
+    void shouldDisplayInfoAlertWhenClickingOnInfoButtonAtListPage() throws InterruptedException {
+        ListPage listPage = new ListPage(driver);
+        HomePage homePage = new HomePage(driver);
+
+        homePage.getListLink().click();
+
+        listPage.getButtonElement().click();
 
         wait.until(ExpectedConditions.alertIsPresent());
         Alert alert = driver.switchTo().alert();
